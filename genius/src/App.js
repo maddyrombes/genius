a9fdc8a98b8fcea2b0934b074e0be37f88175c78
Madeline Rombes <madelinerombes@gmail.com>
2019-08-16 01:35:22 -0400

    more style & functionality

--- genius/src/App.js
+++ genius/src/App.js
@@ -12,16 +12,23 @@ class App extends React.Component {
     super()
     this.state = {
       songs: [],
-      songObjs: []
+      songObjs: [],
+      blocks: []
     }
   };
 
+  // gets all songs from jsonl file and sets it in state
   async componentDidMount() {
     const songs = await fetchAllSongs()
     const splitData = songs.data.split('\n')
     this.setState({ songs: splitData })
   };
 
+
+
+
+  // for Part 1 Q1 -- creates object for each artist
+  // with count of lyric blocks
   countBlocks = () => {
     let songs = this.state.songs;
     let counter = 0;
@@ -46,6 +53,7 @@ class App extends React.Component {
     });
   };
 
+  // for Part 1 Q1 -- basic sort
   sortSongsByBlocks = (a, b) => {
     const artistA = a.numBlocks;
     const artistB = b.numBlocks;
@@ -58,35 +66,87 @@ class App extends React.Component {
     return comparison
   };
 
-  sort = () => {
+  // for Part 1 Q1 -- implements sort and 
+  // gets rid of duplicates
+  reduceSort = () => {
     const sorted = this.state.songObjs.sort(this.sortSongsByBlocks)
     sorted === undefined && (
       console.log(sorted[2])
     )
+    const reduced = Array.from(new Set(sorted.map(a => a.name))).map(id => {
+      return sorted.find(a => a.name === id)
+    })
+    // FINAL ANSWER : Q1
+    // console.log(reduced)
   };
 
 
 
 
-
-  countBlocksSpecificArtists = () => {
+  // for Part 1 Q2 -- 
+  countBlocksSpecificArtists = async () => {
     let songs = this.state.songs;
-    console.log(songs)
     let splitBlocks = []
     songs.forEach((song) => {
-      let artist = song.primary_artist
+      let artist = JSON.parse(song).primary_artist
       JSON.parse(song).lyrics_text.split("[").forEach((block) => {
         let songObj = {};
-        songObj.lyrics = block
+        let lyrics = block.split(": ")[1]
+        if (lyrics) {
+          let splitLyrics = lyrics.split("]")[1]
+          let actualArtist = lyrics.split("]")[0]
+
+          if (actualArtist.includes(", " || "& ")) {
+            // console.log(actualArtist.split(/,|&/))
+            actualArtist.split(/,|&/).forEach((artist) => {
+              songObj.actualArtist = artist
+              songObj.lyrics = splitLyrics
+            })
+          } else {
+            songObj.actualArtist = actualArtist
+            songObj.lyrics = splitLyrics
+          }
+
+        } else {
+          songObj.actualArtist = "none"
+        }
         songObj.primaryArtist = artist
         splitBlocks.push(songObj)
       })
     });
-    console.log(splitBlocks)
+    // console.log(splitBlocks)
+
+    let objArr = []
+    splitBlocks.forEach((block) => {
+      if (block.lyrics) {
+        let sum = splitBlocks.reduce((acc, val) => {
+          return val.actualArtist === block.actualArtist ? acc + 1 : acc
+        }, 0)
+
+
+        let obj = {}
+        obj.total = sum
+        obj.artist = block.actualArtist
+
+        objArr.push(obj)
+      }
+    })
+    const finalCount = Array.from(new Set(objArr.map(a => a.artist)))
+      .map(id => {
+        return objArr.find(a => a.artist === id)
+      })
+
+    const compare = (a, b) => {
+      if (a.total < b.total) {
+        return 1;
+      }
+      if (a.total > b.total) {
+        return -1;
+      }
+      return 0;
+    }
+    // console.log(finalCount.sort(compare))
   };
-
-
-
 
 
 
@@ -94,25 +154,32 @@ class App extends React.Component {
 
   render() {
     this.countBlocks()
-    this.sort()
+    this.reduceSort()
     this.countBlocksSpecificArtists()
 
     return (
       <div className="App">
         <header>
-          <h1>Hire Me,
-          <img className="genius-logo" src={logo} alt="genius logo" />!</h1>
+          <img className="genius-logo" src={logo} alt="genius logo" />
           <nav>
             <li className="nav-item"><Link to="/part1q1" >Part 1 Q1</Link></li>
             <li className="nav-item"><Link to="/part1q2" >Part 1 Q2</Link></li>
             <li className="nav-item"><Link to="/part1q3" >Part 1 Q3</Link></li>
           </nav>
         </header>
-        <main>
+        <>
           <Route path="/part1q1" exact render={() => <MostLyricSections />} />
           <Route path="/part1q2" exact render={() => <PerformedMostSections />} />
           <Route path="/part1q3" exact render={() => <MostUniqueWords />} />
-        </main>
+        </>
+        <footer>
+          <p className="author-name">Maddy Rombes</p>
+          <ul className="footer-nav">
+            <a className="footer-nav-item" href="https://github.com/maddyrombes" target="blank">GitHub</a>
+            <a className="footer-nav-item" href="https://www.linkedin.com/in/maddyrombes/" target="blank">LinkedIn</a>
+            <a className="footer-nav-item" href="http://maddyrombes.surge.sh" target="blank">Portfolio</a>
+          </ul>
+        </footer>
       </div>
     );
   }
